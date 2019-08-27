c28f0387ee0d9ad2445b8a86c307bc08ef27985f
陈绍龙 <chenshaolong@xiandanjia.com>
2019-08-27 16:00:21 +0800

    init

---
+++ app/src/main/java/com/mydagger/demo/App.java
@@ -0,0 +1,16 @@
+package com.mydagger.demo;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by chenshaolong on 2019/8/15.
+ */
+
+public class App extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
